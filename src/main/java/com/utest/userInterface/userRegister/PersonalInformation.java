7af4957011ab0c6e5bbdd36672f344734470e9d6
= <=>
2022-11-26 14:12:25 -0500

    Se mapea la clase HomePage

--- src/main/java/com/utest/userInterface/userRegister/PersonalInformation.java
+++ src/main/java/com/utest/userInterface/userRegister/PersonalInformation.java
@@ -10,6 +10,10 @@ public class PersonalInformation {
     public static final Target FIRST_NAME_INPUT = Target.the("First name input").located(By.id("firstName"));
     public static final Target LAST_NAME_INPUT = Target.the("Last name input").located(By.id("lastName"));
     public static final Target EMAIL_INPUT = Target.the("Email input").located(By.id("email"));
-    public static final Target BIRTH_MONTH_SELECT = Target.the("Birth month").located(By.id(""));
+    public static final Target BIRTH_MONTH_SELECT = Target.the("Birth month select").located(By.id("birthMonth"));
+    public static final Target BIRTH_DAY_SELECT = Target.the("Birth day select").located(By.id("birthDay"));
+    public static final Target BIRTH_YEAR_SELECT = Target.the("Birth year select").located(By.id("birthYear"));
+    public static final Target LANGUAGE_SELECT = Target.the("Language select").located(By.className("ui-select-search input-xs ng-pristine ng-valid ng-empty ng-touched"));
+
 
 }
